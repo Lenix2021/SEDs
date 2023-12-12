12b72038f637fc1bbed4306490fdf2f79414f1ac
Lenix2021 <fianuaaron@gmail.com>
2023-12-11 16:09:59 -0800

    added some transitions to the divs and also worked  on the javascript to do some transition base on the scroll effect

---
+++ JS/about.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Add a class to each box to trigger the transition
+    document.querySelectorAll('.box11').forEach(function(box, index) {
+        box.classList.add('appear');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to check if an element is in the viewport
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Function to handle the intersection observer callback
+    function handleIntersection(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('appear');
+                observer.unobserve(entry.target); // Remove observation once the element is in view
+            }
+        });
+    }
+
+    // Create an intersection observer
+    var observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+    
+
+    // Get all elements with class "offer1" and observe them
+    var offers = document.querySelectorAll('.offer1');
+    offers.forEach(function(offer) {
+        observer.observe(offer);
+    });
+});
--- JS/courses.js
+++ JS/courses.js
@@ -10,3 +10,36 @@ close_sidebar.addEventListener("click",()=>{
    let side=document.getElementById("menu-sidebar")
    side.style.visibility="hidden"
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+   function isInViewport(element) {
+       var rect = element.getBoundingClientRect();
+       return (
+           rect.top >= 0 &&
+           rect.left >= 0 &&
+           rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+           rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+       );
+   }
+
+   function handleIntersection(entries, observer) {
+       entries.forEach(function(entry) {
+           if (entry.isIntersecting) {
+               entry.target.classList.add('appear');
+               observer.unobserve(entry.target);
+           }
+       });
+   }
+
+   var observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+   var programs = document.querySelectorAll('.program1');
+   programs.forEach(function(program) {
+       observer.observe(program);
+   });
+   
+   var othersContents = document.querySelectorAll('.others1');
+   othersContents.forEach(function(othersContent) {
+       observer.observe(othersContent);
+   });
+});
