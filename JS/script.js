4e5878dcffaf2b04d968c3d2ce2793d1edad4f3f
Lenix2021 <fianuaaron@gmail.com>
2024-12-30 05:44:07 -0500

    Worked on the index page

--- JS/script.js
+++ JS/script.js
@@ -50,3 +50,30 @@ words[currentwordindex].style.opacity="1";
     let side=document.getElementById("menu-sidebar")
     side.style.visibility="hidden"
  })
+
+//  popup message to chose the form you are applying for 
+var franko=document.getElementById("franko");
+var close_popup=document.getElementById("close-popup")
+
+franko.addEventListener("click", handlePopup);
+close_popup.addEventListener("click", closePopup);
+
+// Optionally handle touchstart for better mobile support
+franko.addEventListener("touchstart", handlePopup);
+close_popup.addEventListener("touchstart", closePopup);
+
+function handlePopup() {
+  const pop = document.getElementById("popup");
+  pop.style.visibility = "visible";
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
+  overlay.style.pointerEvents = "auto";
+}
+
+function closePopup() {
+  const pop = document.getElementById("popup");
+  pop.style.visibility = "hidden";
+  const over = document.querySelector(".overlay");
+  if (over) document.body.removeChild(over);
+}
